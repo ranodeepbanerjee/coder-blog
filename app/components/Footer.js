56f42c6a9245e145c9a0aeb635961f251a150989
Ranodeep Banerjee <63450189+ranodeepbanerjee@users.noreply.github.com>
2023-06-18 00:11:58 +0530

    Create Footer.js

---
+++ app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200 py-4 text-center">
+      <p className="text-gray-500">Made by Ranodeep Banerjee</p>
+    </footer>
+  );
+};
+
+export default Footer;
